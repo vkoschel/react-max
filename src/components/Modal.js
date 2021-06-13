efd362ed22e74a5397f7631605a4cace9d5bcede
vkoschel <victorkoschel@gmail.com>
2021-06-13 08:16:28 -0300

    First steps with state management

---
+++ src/components/Modal.js
@@ -0,0 +1,20 @@
+function Modal(props) {
+
+  function cancelHandler() {
+
+  }
+
+  function confirmHandler() {
+
+  }
+
+  return (
+    <div className="modal">
+      <p>Are YOu Sure?</p>
+      <button className="btn btn--alt" onClick={cancelHandler}> Cancel</button>
+      <button className="btn" onClick={confirmHandler}> Confirm</button>
+    </div>
+  );
+}
+
+export default Modal;
